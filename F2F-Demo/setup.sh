45888bc0d9eba4d4b9e3def244e7e5b1d8c45592
wscalf <wscalf@redhat.com>
2022-11-02 23:27:36 -0400

    Updated test data to include rolebindings and a team workspace in Aspian

--- F2F-Demo/setup.sh
+++ F2F-Demo/setup.sh
@@ -6,10 +6,18 @@ read kubeconfig
 kubectl kcp workspace root
 
 kubectl kcp workspace create redhat --enter --ignore-existing
-kubectl kcp workspace create hacbs --ignore-existing
 kubectl kcp workspace create appstudio --ignore-existing
 
+kubectl kcp workspace create hacbs --enter --ignore-existing
+kubectl apply -f redhat/hacbs/hacbs-export.yaml
+
 kubectl kcp workspace root
-kubectl kcp workspace create aspian --ignore-existing
+kubectl apply -f aspian/aspian-employees.yaml
+kubectl kcp workspace create aspian --enter --ignore-existing
+kubectl apply -f aspian/entitlement.yaml
+kubectl apply -f aspian/team.yaml
+kubectl kcp workspace create team --ignore-existing
+kubectl kcp workspace ..
+
 kubectl kcp workspace create bspian --ignore-existing
 
