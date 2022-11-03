4325b469b1305c27dd4d60006ba1ac3f0c3bc349
rh-iatanaso <iatanaso@redhat.com>
2022-11-03 10:54:47 -0400

    Add example demo-magic to help running the demo

--- F2F-Demo/setup.sh
+++ F2F-Demo/setup.sh
@@ -1,15 +1,32 @@
-echo "Start kcp ..."  
-read kubeconfig
+#!/bin/sh
 
-#export KUBECONFIG={$kubeconfig}
 
+show_yaml() {
+    if command -v bat &> /dev/null
+    then
+        pe "bat ${1}"
+    else
+        pe "cat ${1}"
+    fi
+}
+
+#shellcheck source=demo-magic.sh
+. demo-magic.sh
+clear 
+
+p "Start kcp ..."
+echo "using KUBECONFIG=$KUBECONFIG"
+echo "Setting up the environment"
+echo "Setting up services workspaces ..."
 kubectl kcp workspace root
 
 kubectl kcp workspace create redhat --enter --ignore-existing
 kubectl kcp workspace create appstudio --ignore-existing
-
 kubectl kcp workspace create hacbs --enter --ignore-existing
+show_yaml "redhat/hacbs/hacbs-export.yaml"
 kubectl apply -f redhat/hacbs/hacbs-export.yaml
+
+echo "Setting up Users/Consumers workspaces ..."
 
 kubectl kcp workspace root
 kubectl apply -f aspian/aspian-employees.yaml
