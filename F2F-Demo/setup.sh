2b66cd9e704c6fd4bb4e9e0416b4f7b7b3521c09
rh-iatanaso <iatanaso@redhat.com>
2022-11-04 16:56:00 -0400

    Add some coloring , add wait

--- F2F-Demo/setup.sh
+++ F2F-Demo/setup.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[91m" # 33 is regular yellow
+ENDCOLOR="\e[0m"
+
+echo_yellow() {
+    echo -e "${YELLOW}${1}${ENDCOLOR}"
+}
 
 show_yaml() {
     if command -v bat &> /dev/null
@@ -15,26 +23,29 @@ source ./demo-magic.sh
 clear 
 
 p "Start kcp ..."
-echo "using KUBECONFIG=$KUBECONFIG"
-echo "Ready to go!"
-echo "First, we set up a fictional redhat organization using the KCP kubectl plugin:"
+echo_yellow "using KUBECONFIG=$KUBECONFIG"
+echo_yellow "Ready to go!"
+echo_yellow "First, we set up a fictional redhat organization using the KCP kubectl plugin:"
+
+wait 
+
 kubectl kcp ws root
 
 kubectl ws create redhat --ignore-existing
 kubectl ws redhat
 
-echo "Then we set up the service provider workspaces for AppStudio and HACBS inside RedHat:"
+echo_yellow "Then we set up the service provider workspaces for AppStudio and HACBS inside RedHat:"
 
 kubectl ws create appstudio --ignore-existing
 kubectl ws appstudio
 kubectl apply -f redhat/appstudio/apiresourceschema-pipelines.appstudio.yaml
-echo "Workspace AppStudio is set up. Now let us look into the APIExport:"
+echo_yellow "Workspace AppStudio is set up. Now let us look into the APIExport:"
 
 show_yaml "redhat/appstudio/appstudio-export.yaml"
-echo "... and apply it for others to bind:"
+echo_yellow "... and apply it for others to bind:"
 kubectl apply -f redhat/appstudio/appstudio-export.yaml
 
-echo "Same goes for the HACBS service provider:"
+echo_yellow "Same goes for the HACBS service provider:"
 kubectl ws ..
 
 kubectl ws create hacbs --ignore-existing
@@ -43,29 +54,29 @@ kubectl apply -f redhat/hacbs/apiresourceschema-pipelines.hacbs.yaml
 show_yaml "redhat/hacbs/hacbs-export.yaml"
 kubectl apply -f redhat/hacbs/hacbs-export.yaml
 
-echo "Now that the Service providers are online, we set up two consumer organizations: Aspian and Bspian."
-echo "Note that these are created using an admin-user:"
-echo ""
+echo_yellow "Now that the Service providers are online, we set up two consumer organizations: Aspian and Bspian."
+echo_yellow "Note that these are created using an admin-user:"
+echo_yellow ""
 kubectl ws root
-echo ""
-echo "Apply the ClusterRole 'aspian-employee' and its binding 'aspian-employees'"
+echo_yellow ""
+echo_yellow "Apply the ClusterRole 'aspian-employee' and its binding 'aspian-employees'"
 pe "kubectl apply -f aspian/aspian-employees.yaml"
-echo ""
-echo "...and create the organizational workspace aspian under root."
+echo_yellow ""
+echo_yellow "...and create the organizational workspace aspian under root."
 pe "kubectl ws create aspian --ignore-existing"
-echo ""
-echo "... then join the workspace and apply the entitlement to bind HACBS to the aspian org."
+echo_yellow ""
+echo_yellow "... then join the workspace and apply the entitlement to bind HACBS to the aspian org."
 kubectl kcp ws aspian
 pe "kubectl apply -f aspian/hacbs-entitlement.yaml"
-echo ""
-echo "... create a 'platform' team role/binding for Aspian:"
+echo_yellow ""
+echo_yellow "... create a 'platform' team role/binding for Aspian:"
 pe "kubectl apply -f aspian/platform-team.yaml"
 
-echo "... and last but not least create the platform team workspace, where HACBS should get bound and used."
+echo_yellow "... and last but not least create the platform team workspace, where HACBS should get bound and used."
 pe "kubectl kcp workspace create platform --ignore-existing"
 kubectl kcp workspace ..
 
-echo "Same goes for Bspian and AppStudio"
+echo_yellow "Same goes for Bspian and AppStudio"
 kubectl apply -f bspian/bspian-employees.yaml
 kubectl ws create bspian --ignore-existing
 kubectl ws bspian
@@ -73,85 +84,87 @@ kubectl apply -f bspian/appstudio-entitlement.yaml
 kubectl apply -f bspian/platform-team.yaml
 kubectl ws create platform --ignore-existing
 kubectl ws ..
-echo "All Service providers and orgs are set up! Ready to go: "
+echo_yellow "All Service providers and orgs are set up! Ready to go: "
 ### Aspian part ####
-echo ""
-echo "Scenario 1: Abigail gives access to the team 'platform', part of aspian org in a workspace called platform for HACBS"
-echo "We just entitled Aspian to bind HACBS, as they bought a subscription. Abigail binds the API to the platform workspace"
-echo ""
+echo_yellow ""
+echo_yellow "Scenario 1: Abigail gives access to the team 'platform', part of aspian org in a workspace called platform for HACBS"
+echo_yellow "We just entitled Aspian to bind HACBS, as they bought a subscription. Abigail binds the API to the platform workspace"
+echo_yellow ""
 
 show_yaml "aspian/team/hacbs-binding.yaml"
 pe "kubectl apply -f ./aspian/team/hacbs-binding.yaml --token abigail"
-echo "Lets look at the bindings for this workspace. We expect the hacbs binding to show up:"
+echo_yellow "Lets look at the bindings for this workspace. We expect the hacbs binding to show up:"
 pe "kubectl get apibindings"
-echo ""
-echo "Now let's create a HACBS pipeline instance to use HACBS in the platform workspace:"
+echo_yellow ""
+echo_yellow "Now let's create a HACBS pipeline instance to use HACBS in the platform workspace:"
 show_yaml "aspian/team/pipeline1.yaml"
 
 pe "kubectl apply -f ./aspian/team/pipeline1.yaml --token abigail"
-echo ""
-echo "Aspian bought a HACBS basic subscription, allowing them to use one pipeline instance only. Let's try to create a second one, which should fail:"
+echo_yellow ""
+echo_yellow "Aspian bought a HACBS basic subscription, allowing them to use one pipeline instance only. Let's try to create a second one, which should fail:"
 show_yaml "aspian/team/pipeline2.yaml"
 pe "kubectl apply -f ./aspian/team/pipeline2.yaml --token abigail"
-echo "Let's look at the existing pipelines. There should not be any new ones:"
+echo_yellow "Let's look at the existing pipelines. There should not be any new ones:"
 pe "kubectl get pipelines"
 
-echo ""
-echo "Scenario 2: Abigail heard of AppStudio and wants to use it. She tries to bind AppStudio for the platform workspace, not knowing if her org purchased a subscription:"
-echo ""
+wait
+
+echo_yellow ""
+echo_yellow "Scenario 2: Abigail heard of AppStudio and wants to use it. She tries to bind AppStudio for the platform workspace, not knowing if her org purchased a subscription:"
+echo_yellow ""
 pe "kubectl kcp ws root:aspian:platform"
 show_yaml "./aspian/team/appstudio-binding.yaml"
 pe "kubectl apply -f ./aspian/team/appstudio-binding.yaml --token abigail"
-echo "Aspian is not entitled to bind AppStudio. Abigail fails to bind this API to the platform workspace."
-echo "Lets look at the bindings. We expect nothing new to be bound:"
+echo_yellow "Aspian is not entitled to bind AppStudio. Abigail fails to bind this API to the platform workspace."
+echo_yellow "Lets look at the bindings. We expect nothing new to be bound:"
 pe "kubectl get apibindings"
-echo ""
-echo "Entitlements are stored at the organization level, they are be plain old kube objects and can be viewed as normal:"
-echo ""
+echo_yellow ""
+echo_yellow "Entitlements are stored at the organization level, they are be plain old kube objects and can be viewed as normal:"
+echo_yellow ""
 pe "kubectl ws root:aspian"
 pe "kubectl get entitlement hacbs -oyaml"
-echo ""
-echo "What if she tries to make superficial changes to this data and create an appstudio entitlement like the following?"
-echo ""
+echo_yellow ""
+echo_yellow "What if she tries to make superficial changes to this data and create an appstudio entitlement like the following?"
+echo_yellow ""
 show_yaml "aspian/appstudio-entitlement.yaml"
-echo "Let's give it a try:"
+echo_yellow "Let's give it a try:"
 pe "kubectl create -f aspian/appstudio-entitlement.yaml --token abigail"
-echo "As we see, an error shows up. That's because Entitlements are protected from modification by normal users."
+echo_yellow "As we see, an error shows up. That's because Entitlements are protected from modification by normal users."
 
-
+wait
 ### Bspian part ###
-echo ""
-echo "Scenario 3: Bspian bought an AppStudio subscription."
-echo "Ben gives access to team 'platform' to use AppStudio. The team is part of the Bspian organisation, in a workspace called platform."
-echo "Bspian is entitled to bind AppStudio, so Ben binds the API to the platform workspace:"
-echo ""
+echo_yellow ""
+echo_yellow "Scenario 3: Bspian bought an AppStudio subscription."
+echo_yellow "Ben gives access to team 'platform' to use AppStudio. The team is part of the Bspian organisation, in a workspace called platform."
+echo_yellow "Bspian is entitled to bind AppStudio, so Ben binds the API to the platform workspace:"
+echo_yellow ""
 
 pe "kubectl kcp ws root:bspian:platform"
 show_yaml "bspian/team/appstudio-binding.yaml"
 ### TODO: should we have another user who cannot apply these bindings cause their token is missing the respective group? ###
 pe "kubectl apply -f ./bspian/team/appstudio-binding.yaml --token ben"
-echo "Lets look at the bindings. We expect the app-studio binding to show up:"
+echo_yellow "Lets look at the bindings. We expect the app-studio binding to show up:"
 pe "kubectl get apibindings"
-echo ""
-echo "Now Ben wants to create an AppStudio App instance to be used:"
+echo_yellow ""
+echo_yellow "Now Ben wants to create an AppStudio App instance to be used:"
 show_yaml "bspian/team/appstudio1.yaml"
 
 pe "kubectl apply -f ./bspian/team/appstudio1.yaml --token ben"
-echo ""
-echo "Bspian has an AppStudio basic subscription, allowing them to use one App only. Let's try to create a second one, which should fail:"
+echo_yellow ""
+echo_yellow "Bspian has an AppStudio basic subscription, allowing them to use one App only. Let's try to create a second one, which should fail:"
 show_yaml "bspian/team/appstudio2.yaml"
 pe "kubectl apply -f ./bspian/team/appstudio2.yaml --token ben"
-echo "Let us look at the existing apps. There should not be any new ones:"
+echo_yellow "Let us look at the existing apps. There should not be any new ones:"
 pe "kubectl get apps"
 
-echo ""
-echo "Ben also heard of HACBS, not knowing if Bspian bought a subscription. He tries to bind HACBS for Bspians platform team: "
-echo ""
+echo_yellow ""
+echo_yellow "Ben also heard of HACBS, not knowing if Bspian bought a subscription. He tries to bind HACBS for Bspians platform team: "
+echo_yellow ""
 pe "kubectl ws root:bspian:platform"
 show_yaml "bspian/team/hacbs-binding.yaml"
 pe "kubectl apply -f ./bspian/team/hacbs-binding.yaml --token ben"
-echo "Bspian is not entitled to Hacbs and ben fails to bind this API to the platform workspace"
-echo "Lets look at the bindings. We expect nothing new to be bound."
+echo_yellow "Bspian is not entitled to Hacbs and ben fails to bind this API to the platform workspace"
+echo_yellow "Lets look at the bindings. We expect nothing new to be bound."
 pe "kubectl get apibindings"
-echo ""
-echo "That was it for now. Thanks for your time! Questions?"
+echo_yellow ""
+echo_yellow "That was it for now. Thanks for your time! Questions?"
