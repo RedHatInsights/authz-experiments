37a8e9f87d504b126416371e9a1ad3cba93c9be5
Dominik Guhr <dguhr@redhat.com>
2022-12-14 12:44:58 +0100

    cleanup of example_test.go to make what's going on here more readable.

--- engine_eval/zanzibar/authz-openfga-ex/test/test_compose/example_test.go
+++ engine_eval/zanzibar/authz-openfga-ex/test/test_compose/example_test.go
@@ -6,16 +6,102 @@ import (
 	openfga "github.com/openfga/go-sdk"
 	"github.com/stretchr/testify/assert"
 	tc "github.com/testcontainers/testcontainers-go"
+	"net/http"
 	"os"
 	"testing"
 )
 
 func TestOpenFgaTcExample(t *testing.T) {
-	//setup compose
 	setupComposeContainers(t)
 
-	// do some testing here
+	storeId := createOpenFgaStoreAndGetId(t)
 
+	apiClient := getApiClientForStore(t, storeId)
+
+	modelData := createAuthorizationModelFromJson(t, apiClient)
+
+	createTuplesFromJson(t, apiClient) // returns the data but not needed here.
+
+	createAssertionsFromJson(t, apiClient, modelData) // returns the data but not needed here.
+
+	//TODO: actually check/assert sth with the given model
+}
+
+func createAssertionsFromJson(t *testing.T, apiClient *openfga.APIClient, modelData openfga.WriteAuthorizationModelResponse) *http.Response {
+	assertions := readAssertionsFromFile(t)
+
+	var assertionsReqBody openfga.WriteAssertionsRequest
+
+	if err := json.Unmarshal(assertions, &assertionsReqBody.Assertions); err != nil {
+		t.Errorf("Error unmarshalling assertions: %v", err)
+	}
+
+	assertionResponse, err := apiClient.OpenFgaApi.WriteAssertions(context.Background(), *modelData.AuthorizationModelId).Body(assertionsReqBody).Execute()
+
+	if err != nil {
+		t.Errorf("Error writing assertions: %v", err)
+	}
+
+	t.Logf("assertion resp: %v", assertionResponse)
+
+	return assertionResponse
+}
+
+func createTuplesFromJson(t *testing.T, apiClient *openfga.APIClient) (map[string]interface{}, *http.Response) {
+	tuples := readTuplesFromFile(t)
+	var tuplesReqBody openfga.WriteRequest
+	var tupleKeys openfga.TupleKeys
+	if err := json.Unmarshal(tuples, &tupleKeys.TupleKeys); err != nil {
+		t.Errorf("Error unmarshalling tuples: %v", err)
+	}
+	tuplesReqBody.SetWrites(tupleKeys)
+	tupleData, tupleResponse, err := apiClient.OpenFgaApi.Write(context.Background()).Body(tuplesReqBody).Execute()
+
+	if err != nil {
+		t.Errorf("Error writing tuples: %v", err)
+	}
+
+	t.Logf("tuple resp data: %v", tupleData)
+	t.Logf("tuple response: %v", tupleResponse)
+	return tupleData, tupleResponse
+}
+
+func createAuthorizationModelFromJson(t *testing.T, apiClient *openfga.APIClient) openfga.WriteAuthorizationModelResponse {
+	model := readModelFromFile(t)
+
+	var modelReqBody openfga.WriteAuthorizationModelRequest
+
+	if err := json.Unmarshal(model, &modelReqBody); err != nil {
+		t.Errorf("Error unmarshalling model: %v", err)
+	}
+
+	modelData, modelResponse, err := apiClient.OpenFgaApi.WriteAuthorizationModel(context.Background()).Body(modelReqBody).Execute()
+
+	if err != nil {
+		t.Errorf("Error writing authorizationmodel: %v", err)
+	}
+
+	t.Logf("model resp data: %v", modelData)
+	t.Logf("model response: %v", modelResponse)
+
+	return modelData
+}
+
+func getApiClientForStore(t *testing.T, storeId string) *openfga.APIClient {
+	configuration, err := openfga.NewConfiguration(openfga.Configuration{
+		ApiScheme: "http",
+		ApiHost:   "0.0.0.0:8080",
+		StoreId:   storeId,
+	})
+
+	if err != nil {
+		t.Errorf("Error creating new configuration for store: %v Error: %v", storeId, err)
+	}
+	apiClient := openfga.NewAPIClient(configuration)
+	return apiClient
+}
+
+func createOpenFgaStoreAndGetId(t *testing.T) string {
 	configuration, err := openfga.NewConfiguration(openfga.Configuration{
 		ApiScheme: "http",
 		ApiHost:   "0.0.0.0:8080",
@@ -37,71 +123,10 @@ func TestOpenFgaTcExample(t *testing.T) {
 
 	t.Logf("Name: %s, ID: %s", resp.GetName(), resp.GetId())
 	storeId := resp.GetId()
-
-	configuration, err = openfga.NewConfiguration(openfga.Configuration{
-		ApiScheme: "http",
-		ApiHost:   "0.0.0.0:8080",
-		StoreId:   storeId,
-	})
-
-	model := readModelFromFile(t, err)
-
-	var modelReqBody openfga.WriteAuthorizationModelRequest
-
-	if err := json.Unmarshal(model, &modelReqBody); err != nil {
-		t.Errorf("Error unmarshalling model: %v", err)
-		return
-	}
-
-	apiClient = openfga.NewAPIClient(configuration)
-
-	modelData, modelResponse, err := apiClient.OpenFgaApi.WriteAuthorizationModel(context.Background()).Body(modelReqBody).Execute()
-	if err != nil {
-		t.Errorf("Error writing authorizationmodel: %v", err)
-	}
-	t.Logf("model resp data: %v", modelData)
-	t.Logf("model response: %v", modelResponse)
-
-	//tuples
-	tuples := readTuplesFromFile(t, err)
-	var tuplesReqBody openfga.WriteRequest
-	var tupleKeys openfga.TupleKeys
-	if err := json.Unmarshal(tuples, &tupleKeys.TupleKeys); err != nil {
-		t.Errorf("Error unmarshalling tuples: %v", err)
-		return
-	}
-	tuplesReqBody.SetWrites(tupleKeys)
-	tupleData, tupleResponse, err := apiClient.OpenFgaApi.Write(context.Background()).Body(tuplesReqBody).Execute()
-
-	if err != nil {
-		t.Errorf("Error writing tuples: %v", err)
-	}
-
-	t.Logf("tuple resp data: %v", tupleData)
-	t.Logf("tuple response: %v", tupleResponse)
-
-	//assertions
-	assertions := readAssertionsFromFile(t, err)
-
-	var assertionsReqBody openfga.WriteAssertionsRequest
-
-	if err := json.Unmarshal(assertions, &assertionsReqBody.Assertions); err != nil {
-		t.Errorf("Error unmarshalling assertions: %v", err)
-		return
-	}
-
-	assertionResponse, err := apiClient.OpenFgaApi.WriteAssertions(context.Background(), *modelData.AuthorizationModelId).Body(assertionsReqBody).Execute()
-
-	if err != nil {
-		t.Errorf("Error writing assertions: %v", err)
-	}
-
-	t.Logf("assertion resp: %v", assertionResponse)
-
-	//TODO: actually check/assert sth with the given model
+	return storeId
 }
 
-func readModelFromFile(t *testing.T, err error) []byte {
+func readModelFromFile(t *testing.T) []byte {
 	model, err := os.ReadFile("model.json")
 
 	if err != nil {
@@ -110,7 +135,7 @@ func readModelFromFile(t *testing.T, err error) []byte {
 	return model
 }
 
-func readTuplesFromFile(t *testing.T, err error) []byte {
+func readTuplesFromFile(t *testing.T) []byte {
 	tuples, err := os.ReadFile("tuples.json")
 
 	if err != nil {
@@ -119,7 +144,7 @@ func readTuplesFromFile(t *testing.T, err error) []byte {
 	return tuples
 }
 
-func readAssertionsFromFile(t *testing.T, err error) []byte {
+func readAssertionsFromFile(t *testing.T) []byte {
 	model, err := os.ReadFile("assertions.json")
 
 	if err != nil {
