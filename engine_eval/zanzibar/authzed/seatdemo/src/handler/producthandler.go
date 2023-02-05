db83b039bd47216575ef0c0a0a14fc57fbdf3c95
Dominik Guhr <dguhr@redhat.com>
2023-02-05 13:17:35 +0100

    Grant endpoint 1/2

--- engine_eval/zanzibar/authzed/seatdemo/src/handler/producthandler.go
+++ engine_eval/zanzibar/authzed/seatdemo/src/handler/producthandler.go
@@ -2,13 +2,15 @@ package handler
 
 import (
 	"context"
-	"errors"
 	v1 "github.com/authzed/authzed-go/proto/authzed/api/v1"
+	"github.com/authzed/authzed-go/v1"
 	"github.com/labstack/echo/v4"
 	"io"
 	"log"
 	"net/http"
 )
+
+var licenseMap = map[string]int{"p1": 5, "p2": 3}
 
 type ProductLicense struct {
 	Name   string `json:"name" xml:"name"`
@@ -16,9 +18,16 @@ type ProductLicense struct {
 	Max    int    `json:"max_seats" xml:"max_seats"`
 }
 
-func GetLicenseInfoForProductInstance(c echo.Context) error {
-	licenseMap := map[string]int{"p1": 5, "p2": 4}
+type GrantLicenseResponse struct {
+	Granted bool   `json:"granted"`
+	Reason  string `json:"reason"`
+}
 
+type GrantLicenseRequest struct {
+	UserId string `form:"userId"`
+}
+
+func GetLicenseInfoForProductInstance(c echo.Context) error {
 	ctx := context.Background()
 	if port == "" {
 		port = "50051" //TODO
@@ -37,19 +46,19 @@ func GetLicenseInfoForProductInstance(c echo.Context) error {
 		ObjectId:   callingName,
 	}}
 
-	tenantId := c.Param("tenant")
-	tenant := &v1.ObjectReference{
+	tId := c.Param("tenant")
+	t1 := &v1.ObjectReference{
 		ObjectType: "tenant",
-		ObjectId:   tenantId,
+		ObjectId:   tId,
 	}
 
-	resp, err := client.CheckPermission(ctx, &v1.CheckPermissionRequest{
-		Resource:   tenant,
+	resp, err2 := client.CheckPermission(ctx, &v1.CheckPermissionRequest{
+		Resource:   t1,
 		Permission: "manage_seats",
 		Subject:    subject,
 	})
 
-	if err != nil {
+	if err2 != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, "uh oh...")
 	}
 
@@ -59,6 +68,124 @@ func GetLicenseInfoForProductInstance(c echo.Context) error {
 	}
 
 	pInstance := c.Param("pinstance")
+	//TODO: check if maxseat value for pInstance exist against map first, if not in map, bad request -> test.
+	currentLicenseCount, err3 := GetCurrentActiveLicenseCountForProductInstance(pInstance, client, ctx)
+	if err3 != nil {
+		return echo.NewHTTPError(http.StatusForbidden, "Internal Server error occured. Please try again later.")
+	}
+
+	result := ProductLicense{
+		Name:   pInstance,
+		Active: currentLicenseCount,
+		Max:    licenseMap[pInstance],
+	}
+	return c.JSON(http.StatusOK, result)
+}
+
+func GrantLicenseIfNotFull(c echo.Context) error {
+	ctx := context.Background()
+	if port == "" {
+		port = "50051" //TODO: remove and refactor
+	}
+
+	//sanity check if requestbody contains userid. send 400 if empty.
+	var grReq GrantLicenseRequest
+	err := c.Bind(&grReq)
+
+	if err != nil || grReq.UserId == "" { //TODO: evaluate why binding in code does not return error when this is empty...
+		return echo.NewHTTPError(http.StatusBadRequest, "Bad Request. User to grant access to needed")
+	}
+
+	client, err2 := getSpiceDbApiClient(port)
+
+	if err2 != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "Internal Server error.")
+	}
+
+	//TODO: this and the following check should be consolidated into one CheckPermissions, change model accordingly, should work, but not now..
+	//check for tenant membership of user to grant stuff for.
+	subj := &v1.SubjectReference{Object: &v1.ObjectReference{
+		ObjectType: "user",
+		ObjectId:   grReq.UserId,
+	}}
+
+	tId := c.Param("tenant")
+	tenant := &v1.ObjectReference{
+		ObjectType: "tenant",
+		ObjectId:   tId,
+	}
+
+	resp, err3 := client.CheckPermission(ctx, &v1.CheckPermissionRequest{
+		Resource:   tenant,
+		Permission: "membership",
+		Subject:    subj,
+	})
+
+	if err3 != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "uh oh.. %s", err3)
+	}
+
+	//check permissions: is user member of tenant?
+	if resp.Permissionship != v1.CheckPermissionResponse_PERMISSIONSHIP_HAS_PERMISSION {
+		return echo.NewHTTPError(http.StatusForbidden, "User "+grReq.UserId+" is not a member of licensed tenant "+tId)
+	}
+
+	subject := &v1.SubjectReference{Object: &v1.ObjectReference{
+		ObjectType: "user",
+		ObjectId:   grReq.UserId,
+	}}
+
+	t2 := &v1.ObjectReference{
+		ObjectType: "user",
+		ObjectId:   grReq.UserId,
+	}
+
+	r, err4 := client.CheckPermission(ctx, &v1.CheckPermissionRequest{
+		Resource:   t2,
+		Permission: "is_not_activated_wsdm_user",
+		Subject:    subject,
+	})
+
+	if err4 != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "uh oh.. "+err4.Error())
+	}
+
+	//check permissions: is user not already an activated wsdm user?!
+	if r.Permissionship != v1.CheckPermissionResponse_PERMISSIONSHIP_HAS_PERMISSION {
+		return echo.NewHTTPError(http.StatusConflict, "Already active license for user "+grReq.UserId+" found.")
+	}
+
+	isFull, err5 := isLicenseFull(c, client, ctx)
+
+	if err5 != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "Internal Server error.")
+	}
+
+	if isFull {
+		result := GrantLicenseResponse{
+			Granted: false,
+			Reason:  "Maximum seats exceeded. Please extend your license.",
+		}
+		//let's have day long discussions around HTTP status codes pls ;)
+		return c.JSON(http.StatusConflict, result)
+	}
+
+	//grant license: set is_activated_wsdm_user to user form postbody
+	return c.JSON(http.StatusOK, "mock")
+}
+
+// sanity check if current < max licenses for tenant. Send 403 + reason: license full.
+func isLicenseFull(c echo.Context, client *authzed.Client, ctx context.Context) (bool, error) {
+	pInstance := c.Param("pinstance")
+	currCount, err := GetCurrentActiveLicenseCountForProductInstance(pInstance, client, ctx)
+	if err != nil {
+		return false, err
+	}
+
+	return currCount >= licenseMap[pInstance]-1, nil
+}
+
+func GetCurrentActiveLicenseCountForProductInstance(pInstance string, client *authzed.Client, ctx context.Context) (int, error) {
 	productInstance := &v1.ObjectReference{
 		ObjectType: "product_instance",
 		ObjectId:   pInstance,
@@ -71,6 +198,11 @@ func GetLicenseInfoForProductInstance(c echo.Context) error {
 		SubjectObjectType: "user",
 	})
 
+	if err != nil {
+		log.Fatal("error fetching licenses using LookupSubject", err)
+		return -1, err
+	}
+
 	var currentLicenseCount int
 	for { //most likely weird and ugly, but works.
 		_, err := stream.Recv()
@@ -82,18 +214,9 @@ func GetLicenseInfoForProductInstance(c echo.Context) error {
 		}
 
 		if err != nil {
-			log.Fatal("tilt", err)
+			log.Fatal("error fetching current licenses", err)
+			return -1, err
 		}
 	}
-
-	result := ProductLicense{
-		Name:   pInstance,
-		Active: currentLicenseCount,
-		Max:    licenseMap[pInstance],
-	}
-	return c.JSON(http.StatusOK, result)
-}
-
-func GrantLicenseIfNotFull(c echo.Context) error {
-	return errors.New("to be implemented")
+	return currentLicenseCount, nil
 }
--- engine_eval/zanzibar/authzed/seatdemo/src/handler/producthandler_test.go
+++ engine_eval/zanzibar/authzed/seatdemo/src/handler/producthandler_test.go
@@ -7,9 +7,109 @@ import (
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
+func TestGrantLicenseReturns403ForUserNotMemberOfTenant(t *testing.T) {
+	ctx := context.Background()
+	/*using one tc instance per test bc don't quite know how to create fixtures and stuff. concious technical debt for now. enlighten me ;)*/
+	db, err := setupSpiceDb(ctx, t)
+	if err != nil {
+		t.Fatalf("container not setup correctly: %s", err)
+	}
+
+	SetPort(db.MappedPort)
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPost, "/tenant/customer1/product/:pinstance/grant", strings.NewReader("userId=t2user3"))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded") //Q: are there no constants for this in go? //TODO: evaluate why binding in code does not return error when this header is not set...
+	rec := httptest.NewRecorder()
+
+	echoCtx := e.NewContext(req, rec)
+	echoCtx.SetPath("/tenant/:tenant/product/:pinstance/grant")
+	echoCtx.SetParamNames("tenant", "pinstance")
+	echoCtx.SetParamValues("customer1", "p1")
+
+	err2 := GrantLicenseIfNotFull(echoCtx)
+	if assert.NotNil(t, err2) {
+		he, ok := err2.(*echo.HTTPError)
+		if ok {
+			assert.Equal(t, http.StatusForbidden, he.Code)
+			assert.Contains(t, he.Message, "User t2user3 is not a member of licensed tenant customer1")
+		}
+	}
+}
+
+func TestGrantLicenseRevokesGrantIfMaxReached(t *testing.T) {
+	ctx := context.Background()
+	/*using one tc instance per test bc don't quite know how to create fixtures and stuff. concious technical debt for now. enlighten me ;)*/
+	db, err := setupSpiceDb(ctx, t)
+	if err != nil {
+		t.Fatalf("container not setup correctly: %s", err)
+	}
+
+	SetPort(db.MappedPort)
+	e := echo.New()
+
+	req := httptest.NewRequest(http.MethodPost, "/tenant/customer1/product/:pinstance/grant", strings.NewReader("userId=t2user3"))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded") //Q: are there no constants for this in go? //TODO: evaluate why binding in code does not return error when this header is not set...
+	rec := httptest.NewRecorder()
+
+	echoCtx := e.NewContext(req, rec)
+	echoCtx.SetPath("/tenant/:tenant/product/:pinstance/grant")
+	echoCtx.SetParamNames("tenant", "pinstance")
+	echoCtx.SetParamValues("customer2", "p2")
+
+	//weird weird way of testing for errors in echo
+	if assert.NoError(t, GrantLicenseIfNotFull(echoCtx)) {
+		assert.Equal(t, http.StatusConflict, rec.Code)
+		ja := jsonassert.New(t)
+		// find some sort of payload
+		granted := false
+		reason := "Maximum seats exceeded. Please extend your license."
+		ja.Assertf(rec.Body.String(), `
+	{
+		"granted": %t,
+		"reason": "%s"
+	}`, granted, reason)
+	}
+}
+
+func TestGrantLicenseReturnsBadRequestWithoutBody(t *testing.T) {
+	ctx := context.Background()
+	/*using one tc instance per test bc don't quite know how to create fixtures and stuff. concious technical debt for now. enlighten me ;)*/
+	db, err := setupSpiceDb(ctx, t)
+	if err != nil {
+		t.Fatalf("container not setup correctly: %s", err)
+	}
+
+	SetPort(db.MappedPort)
+	e := echo.New()
+
+	req := httptest.NewRequest(http.MethodPost, "/tenant/customer1/product/:pinstance/grant", strings.NewReader(""))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded") //Q: are there no constants for this in go?
+	rec := httptest.NewRecorder()
+
+	echoCtx := e.NewContext(req, rec)
+	echoCtx.SetPath("/tenant/:tenant/product/:pinstance/grant")
+	echoCtx.SetParamNames("tenant", "pinstance")
+	echoCtx.SetParamValues("customer1", "p1")
+
+	emptyBodyResp := GrantLicenseIfNotFull(echoCtx)
+
+	if assert.NotNil(t, emptyBodyResp) {
+		he, ok := emptyBodyResp.(*echo.HTTPError)
+		if ok {
+			assert.Equal(t, http.StatusBadRequest, he.Code)
+			assert.Contains(t, he.Message, "Bad Request. User to grant access to needed")
+		}
+	}
+}
+
+/*
+*
+Get Licenses
+*/
 func TestGetLicenseReturnsListOfLicensedUsersForTenant(t *testing.T) {
 	ctx := context.Background()
 	/*using one tc instance per test bc don't quite know how to create fixtures and stuff. concious technical debt for now. enlighten me ;)*/
